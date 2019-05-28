f10fa5df57d7de9bfa9ed63621e2188d1ba997a4
Barnaby Keene <hello@southcla.ws>
2019-05-28 19:27:23 +0100

    add vault path prefix
    This permits the use of a prefix for specifying "directories" for Vault secrets for the running instance of Wadsworth. This facilitates more fine grained control of secret access on different Wadsworth nodes.

--- service/secrets.go
+++ service/secrets.go
@@ -1,6 +1,10 @@
 package service
 
-import "github.com/pkg/errors"
+import (
+	"path/filepath"
+
+	"github.com/pkg/errors"
+)
 
 func (app *App) getSecretsForTarget(name string) (env map[string]string, err error) {
 	if app.vault != nil {
@@ -10,7 +14,7 @@ func (app *App) getSecretsForTarget(name string) (env map[string]string, err err
 }
 
 func (app *App) secretsFromVault(name string) (env map[string]string, err error) {
-	path := "/secret/" + name
+	path := filepath.Join("/secret", app.config.VaultPath, name)
 	secret, err := app.vault.Logical().Read(path)
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to read secret")
