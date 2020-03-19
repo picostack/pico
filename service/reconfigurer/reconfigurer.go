56b4bebcd53b1961e4bd9dae0c0de4c3d1fc6a55
Barnaby Keene <accounts@southcla.ws>
2020-03-19 20:18:08 +0000

    defined the reconfigure provider interface

---
+++ service/reconfigurer/reconfigurer.go
@@ -0,0 +1,11 @@
+package reconfigurer
+
+import (
+	"github.com/picostack/pico/service/watcher"
+)
+
+// Provider describes a type that can provide config state events to a target
+// watcher. It will reconfigure and restart the watcher whenever necessary.
+type Provider interface {
+	Configure(watcher.Watcher)
+}
