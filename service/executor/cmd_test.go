30527017f1e444eeff054a9ef61190d2c739b0c2
Barnaby Keene <accounts@southcla.ws>
2020-03-19 18:55:29 +0000

    written a very basic executor test

---
+++ service/executor/cmd_test.go
@@ -0,0 +1,49 @@
+package executor_test
+
+import (
+	"os"
+	"testing"
+	"time"
+
+	"golang.org/x/sync/errgroup"
+
+	"github.com/picostack/pico/service/executor"
+	"github.com/picostack/pico/service/secret/memory"
+	"github.com/picostack/pico/service/task"
+)
+
+func TestMain(m *testing.M) {
+	os.Mkdir(".test", os.ModePerm)
+	os.Exit(m.Run())
+}
+
+func TestCommandExecutor(t *testing.T) {
+	ce := executor.NewCommandExecutor(&memory.MemorySecrets{})
+	bus := make(chan task.ExecutionTask)
+
+	g := errgroup.Group{}
+
+	g.Go(func() error {
+		bus <- task.ExecutionTask{
+			Target: task.Target{
+				Name: "test_executor",
+				Up:   []string{"touch", "01"},
+			},
+			Path: "./.test",
+		}
+		return nil
+	})
+
+	go ce.Subscribe(bus)
+
+	if err := g.Wait(); err != nil {
+		t.Error(err)
+	}
+
+	// wait for the task to be consumed and executed
+	time.Sleep(time.Second)
+
+	if _, err := os.Stat(".test/01"); err == os.ErrNotExist {
+		t.Error("expected file .test/01 to exist:", err)
+	}
+}
