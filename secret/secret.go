38aba65a13603d252e020feceb505bae62939733
Southclaws <hello@southcla.ws>
2020-03-20 13:19:54 +0000

    document secret

--- secret/secret.go
+++ secret/secret.go
@@ -1,3 +1,6 @@
+// Package secret provides an interface and implementations for secret storage.
+// A secret store is passed to the executor, which hydrates execution tasks with
+// any secrets that match it.
 package secret
 
 // Store describes a type that can securely obtain secrets for services.
