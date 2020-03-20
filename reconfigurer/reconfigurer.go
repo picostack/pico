e2d1b7429fa458aa542ebbedd48550ba130ee30a
Southclaws <hello@southcla.ws>
2020-03-20 13:23:44 +0000

    document reconfigurer

--- reconfigurer/reconfigurer.go
+++ reconfigurer/reconfigurer.go
@@ -1,3 +1,10 @@
+// Package reconfigurer is responsible for providing configuration state to a
+// watcher. It may acquire that state from somewhere such as a Git repository or
+// an API. When state needs to be updated, a configuration provider will call
+// the Watcher's SetState method. The state transition is then the reponsibility
+// of the watcher. All a configuration provider does is acquire configuration
+// from some source and ensure it's valid. Providers may store a fallback state
+// if validation fails to ensure reliability.
 package reconfigurer
 
 import (
