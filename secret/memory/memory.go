c97044b84f7225dec1af4f22fd63da56f19d500d
Barnaby Keene <accounts@southcla.ws>
2020-03-24 23:26:30 +0000

    Vault arbitrary env passthrough (#52)
    * resolve #46
    
    Adds a new option: pass-env, which when true will pass the pico process environment to children. Defaults to false to promote separation of environments.
    
    Adds support for passing prefixed variables from the global Pico. The prefix is GLOBAL_ and is not configurable because I felt the config flags are growing.
    
    Adds some better unit tests for execution config and environment merging.
    
    * correctly strip the prefix for global configuration values pulled from the secret store

--- secret/memory/memory.go
+++ secret/memory/memory.go
@@ -6,12 +6,16 @@ import (
 
 // MemorySecrets implements a simple in-memory secret.Store for testing
 type MemorySecrets struct {
-	Secrets map[string]string
+	Secrets map[string]map[string]string
 }
 
 var _ secret.Store = &MemorySecrets{}
 
 // GetSecretsForTarget implements secret.Store
 func (v *MemorySecrets) GetSecretsForTarget(name string) (map[string]string, error) {
-	return v.Secrets, nil
+	table, ok := v.Secrets[name]
+	if !ok {
+		return nil, nil
+	}
+	return table, nil
 }
