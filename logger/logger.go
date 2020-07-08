0b57e2e34dfa69dc9f8b9b7f6068f325d24d7e41
Southclaws <hello@southcla.ws>
2020-07-08 18:42:51 +0100

    improved the logger configuration with structured environment variables and improved feedback

--- logger/logger.go
+++ logger/logger.go
@@ -3,37 +3,53 @@ package logger
 import (
 	"fmt"
 	"os"
-	"strconv"
 	"strings"
 
 	"github.com/joho/godotenv"
+	"github.com/kelseyhightower/envconfig"
+	"github.com/pkg/errors"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
 
+type Env string
+
+type cfg struct {
+	Environment Env           `envconfig:"ENVIRONMENT" default:"production"`
+	LogLevel    zapcore.Level `envconfig:"LOG_LEVEL"   default:"info"`
+}
+
+const (
+	EnvironmentProd Env = "production"
+	EnvironmentDev  Env = "development"
+)
+
+func (e *Env) UnmarshalText(text []byte) error {
+	switch strings.ToLower(string(text)) {
+	case string(EnvironmentProd):
+		*e = EnvironmentProd
+	case string(EnvironmentDev):
+		*e = EnvironmentDev
+	default:
+		return errors.Errorf("unknown environment type: '%s'", string(text))
+	}
+	return nil
+}
+
 func init() {
 	godotenv.Load("../.env", ".env") //nolint:errcheck
 
-	prod, err := strconv.ParseBool(os.Getenv("PRODUCTION"))
-	if _, ok := err.(*strconv.NumError); !ok {
-		fmt.Println("Error during logging config:", err)
-		os.Exit(1)
-	}
+	var c cfg
+	envconfig.MustProcess("", &c)
 
 	var config zap.Config
-	if prod && !isInTests() {
-		config = zap.NewProductionConfig()
-	} else {
+	if c.Environment == EnvironmentDev || isInTests() {
 		config = zap.NewDevelopmentConfig()
+	} else {
+		config = zap.NewProductionConfig()
 	}
 
-	var level zapcore.Level
-	if err := level.UnmarshalText([]byte(os.Getenv("LOG_LEVEL"))); err != nil {
-		fmt.Println("Error during logging config:", err)
-		os.Exit(1)
-	}
-
-	config.Level.SetLevel(level)
+	config.Level.SetLevel(c.LogLevel)
 	config.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
 
 	logger, err := config.Build()
@@ -43,9 +59,10 @@ func init() {
 	}
 	zap.ReplaceGlobals(logger)
 
-	if !prod {
-		zap.L().Info("logger configured in development mode", zap.String("level", level.String()))
-	}
+	zap.L().Info("logger configured",
+		zap.String("level", c.LogLevel.String()),
+		zap.String("env", string(c.Environment)))
+
 }
 
 func isInTests() bool {
